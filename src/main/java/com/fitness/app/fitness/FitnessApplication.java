9aea71ed20cfe1f8eb56a39c320b5c85d1a76f5a
carlos <446329860@qq.com>
2024-04-07 17:28:39 +0800

    Version 1.0.0

--- src/main/java/com/fitness/app/fitness/FitnessApplication.java
+++ src/main/java/com/fitness/app/fitness/FitnessApplication.java
@@ -1,13 +1,23 @@
 package com.fitness.app.fitness;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+
+@Slf4j
+@MapperScan("com.fitness.app.fitness.repository")
 @SpringBootApplication
 public class FitnessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FitnessApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(FitnessApplication.class);
+        Environment env = springApplication.run(args).getEnvironment();
+        log.info("Fitness modules server has started : {}, CPU core : {}"
+                , Arrays.toString(env.getActiveProfiles()), Runtime.getRuntime().availableProcessors());
     }
 
 }
